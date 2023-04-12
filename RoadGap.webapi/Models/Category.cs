37daf618c8a404787d6fd63f0a9714e4fb5c5b85
IvanRozb <ivanrozb4225@gmail.com>
2023-04-12 18:05:45 +0300

    Add model - Category

---
+++ RoadGap.webapi/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace RoadGap.webapi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string Title { get; set; } = "";
+    public string Photo { get; set; } = "";
+}
