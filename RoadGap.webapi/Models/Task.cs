34b89802bc30a45ea9b73691b5362cdcf9279e7e
IvanRozb <ivanrozb4225@gmail.com>
2023-04-12 18:02:14 +0300

    Create model Task

---
+++ RoadGap.webapi/Models/Task.cs
@@ -0,0 +1,13 @@
+namespace RoadGap.webapi.Models;
+
+public class Task
+{
+    public int TaskId { get; set; }
+    public int CategoryId { get; set; }
+    public int StatusId { get; set; }
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public DateTime StartTime { get; set; }
+    public DateTime Deadline { get; set; }
+    public DateTime TaskUpdated { get; set; }
+}
