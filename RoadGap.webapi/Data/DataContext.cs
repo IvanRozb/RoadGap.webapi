d410e00acfe4453992a63393fbe5fe43a1fb22ba
IvanRozb <ivanrozb4225@gmail.com>
2023-04-12 18:22:00 +0300

    EntityFramework setup

--- RoadGap.webapi/Data/DataContext.cs
+++ RoadGap.webapi/Data/DataContext.cs
@@ -1,14 +1,38 @@
 using Microsoft.EntityFrameworkCore;
+using RoadGap.webapi.Models;
+using Task = RoadGap.webapi.Models.Task;
 
 namespace RoadGap.webapi.Data;
 
 public class DataContext : DbContext
 {
     private readonly IConfiguration _configuration;
+
     public DataContext(IConfiguration configuration)
     {
         _configuration = configuration;
     }
-    
-    
+
+    public virtual DbSet<Task> Tasks { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Status> Statuses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"), 
+                builder => builder.EnableRetryOnFailure()
+            );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>()
+            .ToTable("Tasks")
+            .HasKey();
+        modelBuilder.Entity<Category>()
+            .HasKey();
+        modelBuilder.Entity<Status>()
+            .HasKey();
+    }
 }
