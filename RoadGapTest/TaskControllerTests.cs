702f3555266ce0cb876f1d6e1efc1bc826b22952
IvanRozb <ivanrozb4225@gmail.com>
2023-04-13 16:09:13 +0300

    Updated setup for tests

---
+++ RoadGapTest/TaskControllerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Configuration;
+using RoadGap.webapi.Controllers;
+
+namespace RoadGapTest;
+
+[TestFixture]
+public class TaskControllerTests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.That(1, Is.EqualTo(1));
+    }
+}
