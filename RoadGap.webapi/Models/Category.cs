85b95a59452012fd2df36169f5cc04ad11be28c4
IvanRozb <ivanrozb4225@gmail.com>
2023-04-15 17:07:45 +0300

    Add Description for Category

--- RoadGap.webapi/Models/Category.cs
+++ RoadGap.webapi/Models/Category.cs
@@ -8,4 +8,5 @@ public class Category
     public int CategoryId { get; set; }
     public string Title { get; set; } = "";
     public string Photo { get; set; } = "";
+    public string Description { get; set; } = "";
 }
