a8556154b6f375ec05aed7c33e63555887b63130
IvanRozb <ivanrozb4225@gmail.com>
2023-04-17 12:01:48 +0300

    Refactor CategoryRepository to inherit from Repository class

--- RoadGap.webapi/Repositories/Implementation/CategoryRepository.cs
+++ RoadGap.webapi/Repositories/Implementation/CategoryRepository.cs
@@ -1,55 +1,25 @@
-using RoadGap.webapi.Data;
+using AutoMapper;
 using RoadGap.webapi.Models;
 
 namespace RoadGap.webapi.Repositories.Implementation;
 
-public class CategoryRepository : ICategoryRepository
+public class CategoryRepository : Repository, ICategoryRepository
 {
-    private readonly DataContext _entityFramework;
-
-    public CategoryRepository(IConfiguration configuration)
+    public CategoryRepository(IConfiguration configuration, IMapper mapper)
+        : base(configuration, mapper)
     {
-        _entityFramework = new DataContext(configuration);
-    }
-
-    public CategoryRepository(DataContext context)
-    {
-        _entityFramework = context;
     }
     public void Dispose()
     {
-        _entityFramework.Category.RemoveRange(_entityFramework.Category);
+        EntityFramework.Category.RemoveRange(EntityFramework.Category);
         SaveChanges();
     }
-
-    public DataContext GetDataContext()
-    {
-        return _entityFramework;
-    }
-    public void SaveChanges()
-    {
-        if (_entityFramework.SaveChanges() < 0)
-            throw new Exception("Failed to save changes to database");
-    }
-
-    public void AddEntity<T>(T entity)
-    {
-        if (entity == null) return;
-        _entityFramework.Add(entity);
-    }
-    
-    public void RemoveEntity<T>(T entity)
-    {
-        if (entity == null) return;
-        _entityFramework.Remove(entity);
-    }
-    
     public IEnumerable<Category> GetCategories() =>
-        _entityFramework.Category.ToList();
+        EntityFramework.Category.ToList();
 
     public Category? GetCategoryById(int categoryId)
     {
-        var category = _entityFramework.Category
+        var category = EntityFramework.Category
             .FirstOrDefault(category => category.CategoryId == categoryId);
         
         return category;
@@ -58,7 +28,7 @@ public class CategoryRepository : ICategoryRepository
     public IEnumerable<Category> GetCategoriesBySearch(string searchParam)
     {
         var keywords = searchParam.ToLower().Split(' ');
-        var categories = _entityFramework.Category;
+        var categories = EntityFramework.Category;
         var searchedCategories = new List<Category>();
         
         foreach (var keyword in keywords)
