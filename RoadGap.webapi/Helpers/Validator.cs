a0d706287a87c22fcfda568d98c09f583ebb415d
IvanRozb <ivanrozb4225@gmail.com>
2023-04-18 20:13:01 +0300

    Added length validation for inputs in Validator

--- RoadGap.webapi/Helpers/Validator.cs
+++ RoadGap.webapi/Helpers/Validator.cs
@@ -6,20 +6,18 @@ public static partial class Validator
 {
     public static bool ValidateUserName(string userName)
     {
-        // Match the input string against the pattern
-        return MyRegex().IsMatch(userName);
+        return userName.Length <= 50 && MyRegex().IsMatch(userName);
     }
 
     public static bool ValidateEmail(string email)
     {
-        // Match the input string against the pattern
-        return MyRegex1().IsMatch(email);
+        return email.Length <= 320 && MyRegex1().IsMatch(email);
     }
 
     // Regex pattern to match only alphabets and numbers
     [GeneratedRegex("^[a-zA-Z0-9]*$")]
     private static partial Regex MyRegex();
-    
+
     // Regex pattern to match email format
     [GeneratedRegex("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$")]
     private static partial Regex MyRegex1();
