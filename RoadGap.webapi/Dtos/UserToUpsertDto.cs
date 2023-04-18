febb2c430df4bbb99b6d82a9ea604e0f0eacea15
IvanRozb <ivanrozb4225@gmail.com>
2023-04-18 15:28:21 +0300

    Added other endpoints

---
+++ RoadGap.webapi/Dtos/UserToUpsertDto.cs
@@ -0,0 +1,8 @@
+namespace RoadGap.webapi.Dtos;
+
+public class UserToUpsertDto
+{
+    public string UserName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Photo { get; set; } = "";
+}
