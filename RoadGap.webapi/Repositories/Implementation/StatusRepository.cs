66a34b5126251644978793f01cce86fee748c78d
IvanRozb <ivanrozb4225@gmail.com>
2023-04-19 11:21:13 +0300

    Refactored Status CRUD operations to handle duplicates while creating or updating entities

--- RoadGap.webapi/Repositories/Implementation/StatusRepository.cs
+++ RoadGap.webapi/Repositories/Implementation/StatusRepository.cs
@@ -12,39 +12,56 @@ public class StatusRepository : Repository, IStatusRepository
         : base(configuration, mapper)
     {
     }
-    
-    public StatusRepository(DataContext context, IMapper mapper) 
+
+    public StatusRepository(DataContext context, IMapper mapper)
         : base(context, mapper)
     {
     }
-    
+
     public void Dispose()
     {
         EntityFramework.Status.RemoveRange(EntityFramework.Status);
         SaveChanges();
     }
-    
+
     public RepositoryResponse<IEnumerable<Status>> GetStatuses()
     {
         bool SearchPredicate(Status status) => true;
 
-        return SearchEntities((Func<Status, bool>)SearchPredicate, "Statuses found successfully.", "An error occurred while getting statuses.");
+        return SearchEntities((Func<Status, bool>)SearchPredicate,
+            "Statuses found successfully.",
+            "An error occurred while getting statuses.");
     }
+
     public RepositoryResponse<Status> GetStatusById(int statusId)
     {
         return GetEntityById<Status>(statusId);
     }
 
-    public RepositoryResponse<Status> EditStatus(int statusId, StatusToUpsertDto statusDto)
+    public RepositoryResponse<Status> EditStatus(int statusId, StatusToUpsertDto statusToEdit)
     {
-        return EditEntity(statusId, statusDto, GetStatusById);
+        if (EntityChecker.StatusExistsByTitle(statusToEdit.Title))
+        {
+            return RepositoryResponse<Status>
+                .CreateConflict("The status with this title already exists");
+        }
+
+        return EditEntity(statusId, statusToEdit, GetStatusById);
     }
 
     public RepositoryResponse<Status> CreateStatus(StatusToUpsertDto statusToAdd)
     {
+        if (EntityChecker.StatusExistsByTitle(statusToAdd.Title))
+        {
+            return RepositoryResponse<Status>
+                .CreateConflict("The status with this title already exists");
+        }
+        
         return CreateEntity<Status, StatusToUpsertDto>(statusToAdd);
     }
+
     public RepositoryResponse<int> DeleteStatus(int statusId)
     {
         return DeleteEntity<Status>(statusId);
-    }}
+    }
+}
