438b0b6c71c1c31174b27387f2aa29c4f988715c
IvanRozb <ivanrozb4225@gmail.com>
2023-04-19 12:28:54 +0300

    Add url validation and checking empty strings

--- RoadGap.webapi/Helpers/Validator.cs
+++ RoadGap.webapi/Helpers/Validator.cs
@@ -6,19 +6,38 @@ public static partial class Validator
 {
     public static bool ValidateUserName(string userName)
     {
-        return userName.Length <= 50 && MyRegex().IsMatch(userName);
+        var trimmedUserName = userName.Trim();
+        return !string.IsNullOrEmpty(userName)
+               && userName.Length <= 50
+               && MyRegex().IsMatch(trimmedUserName);
     }
 
     public static bool ValidateEmail(string email)
     {
-        return email.Length <= 320 && MyRegex1().IsMatch(email);
+        var trimmedEmail = email.Trim();
+        return !string.IsNullOrEmpty(trimmedEmail)
+               && trimmedEmail.Length <= 320
+               && MyRegex1().IsMatch(trimmedEmail);
+    }
+
+    public static bool ValidateUrl(string url)
+    {
+        var trimmedUrl = url.Trim();
+        return !string.IsNullOrEmpty(trimmedUrl)
+               && trimmedUrl.Length <= 255
+               && MyRegex2().IsMatch(trimmedUrl);
     }
 
     // Regex pattern to match only alphabets and numbers
-    [GeneratedRegex("^[a-zA-Z0-9]*$")]
+    [GeneratedRegex("^[a-zA-Z0-9]+$")]
     private static partial Regex MyRegex();
 
     // Regex pattern to match email format
     [GeneratedRegex("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$")]
     private static partial Regex MyRegex1();
+
+    // Regex pattern to match url format
+    [GeneratedRegex(
+        "^(http|https):\\/\\/[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?$")]
+    private static partial Regex MyRegex2();
 }
