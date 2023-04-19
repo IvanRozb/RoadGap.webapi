1adf7fb3206df763dde8c01f070391410aea8bd5
IvanRozb <ivanrozb4225@gmail.com>
2023-04-19 19:54:04 +0300

    Created extension for ConfigureExceptionHandler in ExceptionMiddlewareExtensions class

---
+++ RoadGap.webapi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using LoggerService;
+using Microsoft.AspNetCore.Diagnostics;
+using RoadGap.webapi.Models;
+
+namespace RoadGap.webapi.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureExceptionHandler(
+        this IApplicationBuilder app,
+        ILoggerManager logger)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature is not null)
+                {
+                    logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = "Internal Server Error."
+                    }.ToString());
+                }
+            });
+        });
+    }
+}
