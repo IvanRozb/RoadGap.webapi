bf0e35f9120fdfe886e1b65eb817076d212889d9
IvanRozb <ivanrozb4225@gmail.com>
2023-04-21 15:59:55 +0300

    Created User's DTOs

---
+++ Contracts/UserDto.cs
@@ -0,0 +1,17 @@
+using Domain.Entities;
+
+namespace Contracts;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public IEnumerable<TaskModel> Tasks { get; set; }
+    public IEnumerable<Project> Projects { get; set; }
+}
